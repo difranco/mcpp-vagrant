0e8d337f38d8b9a506e23443ecf538cd37e77c10
Anthony Di Franco <di.franco@gmail.com>
2016-12-15 14:18:07 -0800

    fix dynamics by using reference parameters to update multiple state variables in dynamics loop

--- funarc-test/main.cpp
+++ funarc-test/main.cpp
@@ -33,17 +33,16 @@ typedef mc::TVar<MC> TVMC;
 using namespace std;
 using namespace mc;
 
-TVMC gravity( TVMC x1_0, long double m1, TVMC x2_0, long double m2, long double time)
+void gravity( TVMC& x1, TVMC& x1v, long double m1, TVMC x2, long double m2, long double time)
 // returns new position of x1 computed by Euler integration
 // positions in meters, masses in kg, time in seconds
 {
   long double G = 6.6740831e-11;
-
   // force on x1
-  TVMC r = x1_0 - x2_0;
+  TVMC r = x1 - x2;
   TVMC a1 = G*m2/((r)*(r));
-  TVMC x1 = x1_0 + a1 * time;
-  return x1;
+  x1v = x1v + a1 * time;
+  x1 = x1 + x1v * time;
 }
 
 int main()
@@ -63,18 +62,20 @@ int main()
     double earth_radius = 6.371e6;
     double initial_condition[2] = { 0.1, earth_radius + 1.0 };
     // We construct a model with 2 variables, one for the Earth and another for a dropped ball
-    TMMC model( 2, 3 );
+    TMMC model( 3, 2 );
     TVMC earth_position_0( &model, 0, MC( I(-1.0, 1.0), 0.0 ) );
-    TVMC ball_position_0( &model, 1, MC( I(earth_radius - 1.0, earth_radius + 10.0), earth_radius ) );
+    TVMC ball_position_0 = TVMC( &model, 1, MC( I(earth_radius - 1.0, earth_radius + 10.0), earth_radius ) );
+    TVMC ball_velocity_0 = TVMC( &model, 2, MC( I(-1, 1), 0.0 ) );
     TVMC ball_position = ball_position_0;
+    TVMC ball_velocity = ball_velocity_0;
 
     long double earth_mass = 5.972e24;
     long double ball_mass = 0.1;
 
     for (int i = 0; i < 10; i++) {
-        cout << "Approximation of ball position at second " << i << ": " << ball_position << endl;
-        cout << "Evaluated approximation of ball position at second " << i << ": " << ball_position.P(initial_condition) << endl;
-        ball_position = gravity(ball_position, ball_mass, earth_position_0, earth_mass, 1.0);
+        cout << "Approximation of ball p/v at second " << i << ": " << ball_position << endl << ball_velocity << endl;
+        cout << "Evaluated ball p/v at second " << i << ": " << endl << ball_position.P(initial_condition) << endl << ball_velocity.P(initial_condition) << endl;
+        gravity(ball_position, ball_velocity, ball_mass, earth_position_0, earth_mass, 1.0);
     }
     // evaluate position for initial conditions of ball at earth radius + 1 and earth at zero
     long double result = ball_position.P(initial_condition);
